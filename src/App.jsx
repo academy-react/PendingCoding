065257fbfdd5e7688dfe0acd9e82cd262318b5ef
a_abbas9684 <devilabbasbabaii@gmail.com>
2023-10-03 18:57:23 +0330

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 function App() {
-  return <main className="text-3xl text-emerald-700">Hello project!</main>;
+  return <main className="text-3xl text-emerald-700">start project!</main>;
 }
 
 export default App;
