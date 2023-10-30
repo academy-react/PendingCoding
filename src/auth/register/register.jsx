a119992a7a25c6446323607c6c8c98fc2ee3a39d
arman-rv <pendingcoding@gmail.com>
2023-10-31 02:20:19 +0330

    finish multi step form

--- src/auth/register/register.jsx
+++ src/auth/register/register.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "../../../libs/utils";
+import { useState } from "react";
 
 const Register = () => {
   const formSchema = z
@@ -18,6 +19,14 @@ const Register = () => {
         .string()
         .min(4, { message: "رمز عبور حداقل 4 کاراکتر دارد" })
         .max(15, { message: "رمز عبور حداکثر 15 کاراکتر دارد" }),
+      phoneNumber: z
+        .string({ message: "شماره خود را وارد کنید" })
+        .min(11, { message: "تعداد شماره باید 11 تا باشد" })
+        .max(11, { message: "تعداد شماره باید 11 تا باشد" }),
+      idNumber: z
+        .string({ message: "کد ملی خود را وارد کنید" })
+        .min(10, { message: "تعداد ارقام باید 10 تا باشد" })
+        .max(10, { message: "تعداد ارقام باید 10 تا باشد" }),
     })
     .refine((data) => data.password === data.confirmPassword, {
       message: "باید با رمز عبور یکسان باشد",
@@ -34,7 +43,22 @@ const Register = () => {
 
   const onSubmit = (values) => {
     console.log("form updated...!!!", values);
+    // return(
+    //   <div className="text-[18px] text-[#505050] p-[10px_20px] border-[1px] border-green-600 border-solid">ثبت نام موفقیت امیز بود</div>
+    // )
   };
+
+  // steps function
+
+  const [step , setStep] = useState(1);
+
+  const handleNext = () => {
+    setStep(step + 1);
+  }
+
+  const handleBack = () => {
+    setStep(step - 1);
+  }
 
   return (
     <div
@@ -47,117 +71,203 @@ const Register = () => {
          
          max-[700px]:w-[275px]"
       >
-        <h1
-          className="text-[#505050] text-[28px] mb-[40px]
-             
-            max-[700px]:mb-[10px] max-[700px]:text-[26px]"
-        >
-          {" "}
-          ثبت نام حساب کاربری{" "}
-        </h1>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="w-[100%]">
-          <input
-            type="text"
-            placeholder="پست الکترونیکی"
-            className={cn(
-              `focus:outline-none focus:border-[#989898] block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
-                 
-              max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
-              errors.email && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434]`
-            )}
-            {...register("email")}
-          />
-          {/* width to 100% */}
+        {step === 1 && (
+        <>
 
-          {errors.email && (
-            <div className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
-            
-            max-[700px]:text-[13px] max-[700px]:bottom-[12px]">
-              {errors.email?.message}
-            </div>
-          )}
-
-          <input
-            type="password"
-            placeholder="رمز عبور"
-            className={cn(
-              `focus:outline-none focus:border-[#989898]  block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
-                 
-              max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
-              errors.password && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434]`
-            )}
-            {...register("password")}
-          />
-          {errors.password && (
-            <div className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
-            
-            max-[700px]:text-[13px] max-[700px]:bottom-[12px]">
-              {errors.password?.message}
-            </div>
-          )}
-
-          <input
-            type="password"
-            placeholder="تکرار رمز عبور"
-            className={cn(
-              `focus:outline-none focus:border-[#989898]  block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
-                   
-              max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
-              errors.confirmPassword && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434]`
-            )}
-            {...register("confirmPassword")}
-          />
-          {errors.confirmPassword && (
-            <div className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
-            
-            max-[700px]:text-[13px] max-[700px]:bottom-[12px]">
-              {errors.confirmPassword?.message}
-            </div>
-          )}
-
-          <div
-            className="flex gap-[10px] m-[0_0_20px] items-center
-                     
-                     max-[700px]:m-[0_0_13px]"
-          >
-            <input
-              type="checkbox"
-              name="term"
-              id="term"
-              className="absolute invisible peer"
-            />
-
-            <label
-              htmlFor="term"
-              className="w-[24px] h-[24px] bg-white shadow-[0_1px_10px_rgba(0,0,0,0.25)] rounded-[7px] peer-checked:shadow-[0_1px_10px_rgba(92,85,201,0.25)] peer-checked:bg-[url('../../public/img/check.svg')] bg-no-repeat bg-[length:75%] bg-[50%] cursor-pointer
-                         
-              max-[700px]:w-[20px] max-[700px]:h-[20px]"
-            >
-              {" "}
-            </label>
-
-            <label
-              htmlFor="term"
-              className="text-[12px] text-[#969696] cursor-pointer"
-            >
-              {" "}
-              با <a className="text-[#02B2B8]">قوانین و مقررات </a>درج شده در
-              آکادمی موافقم.{" "}
-            </label>
-          </div>
-
-          <button
-            className="
-            bg-[#505050] cursor-pointer rounded-[50px] text-[18px] text-white w-[100%] p-[10px_0]
-                 
-            max-[700px]:p-[7px_0]"
-            type="submit"
+          <h1
+            className="text-[#505050] text-[28px] mb-[40px]
+              
+              max-[700px]:mb-[10px] max-[700px]:text-[26px]"
           >
             {" "}
-            مرحله بعد{" "}
-          </button>
-        </form>
+            ثبت نام حساب کاربری{" "}
+          </h1>
+
+          <form className="w-[100%] flex flex-col">
+            <input
+              type="text"
+              placeholder="پست الکترونیکی"
+              className={cn(
+                `focus:outline-none focus:border-[#989898] block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
+                  
+                max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
+                errors.email && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434] focus:border-[#ff3b3b]`
+              )}
+              {...register("email")}
+            />
+
+            {errors.email && (
+              <div className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
+              
+              max-[700px]:text-[13px] max-[700px]:bottom-[12px]">
+                {errors.email?.message}
+              </div>
+            )}
+
+            <input
+              type="password"
+              placeholder="رمز عبور"
+              className={cn(
+                `focus:outline-none focus:border-[#989898]  block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
+                  
+                max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
+                errors.password && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434] focus:border-[#ff3b3b]`
+              )}
+              {...register("password")}
+            />
+            {errors.password && (
+              <div className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
+              
+              max-[700px]:text-[13px] max-[700px]:bottom-[12px]">
+                {errors.password?.message}
+              </div>
+            )}
+
+            <input
+              type="password"
+              placeholder="تکرار رمز عبور"
+              className={cn(
+                `focus:outline-none focus:border-[#989898]  block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
+                    
+                max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
+                errors.confirmPassword && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434] focus:border-[#ff3b3b]`
+              )}
+              {...register("confirmPassword")}
+            />
+            {errors.confirmPassword && (
+              <div className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
+              
+              max-[700px]:text-[13px] max-[700px]:bottom-[12px]">
+                {errors.confirmPassword?.message}
+              </div>
+            )}
+
+            <div
+              className="flex gap-[10px] m-[0_0_20px] items-center
+                      
+                      max-[700px]:m-[0_0_13px]"
+            >
+              <input
+                type="checkbox"
+                name="term"
+                id="term"
+                className="absolute invisible peer"
+              />
+
+              <label
+                htmlFor="term"
+                className="w-[24px] h-[24px] bg-white shadow-[0_1px_10px_rgba(0,0,0,0.25)] rounded-[7px] peer-checked:shadow-[0_1px_10px_rgba(92,85,201,0.25)] peer-checked:bg-[url('../../public/img/check.svg')] bg-no-repeat bg-[length:75%] bg-[50%] cursor-pointer
+                          
+                max-[700px]:w-[20px] max-[700px]:h-[20px]"
+              >
+                {" "}
+              </label>
+
+              <label
+                htmlFor="term"
+                className="text-[12px] text-[#969696] cursor-pointer"
+              >
+                {" "}
+                با <a className="text-[#02B2B8]">قوانین و مقررات </a>درج شده در
+                آکادمی موافقم.{" "}
+              </label>
+            </div>
+
+            <button
+              onClick={handleNext}
+              className="
+              bg-[#505050] cursor-pointer rounded-[50px] text-[18px] text-white w-[100%] p-[10px_0]
+                  
+              max-[700px]:p-[7px_0]"
+              type="submit"
+            >
+              {" "}
+              مرحله بعد{" "}
+            </button>
+          </form>
+
+        </>
+        )}
+
+        {step === 2 && (
+          <>
+
+            <h1
+              className="text-[#505050] text-[28px] mb-[40px]
+                
+                max-[700px]:mb-[10px] max-[700px]:text-[26px]"
+            >
+              {" "}
+              ثبت نام حساب کاربری{" "}
+            </h1>
+  
+          <form
+            onSubmit={handleSubmit(onSubmit)} 
+            className="w-[100%] flex flex-col"
+            >
+    
+              <input
+              type="text"
+              placeholder="شماره موبایل"
+              className={cn(
+                `focus:outline-none focus:border-[#989898]  block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
+                    
+                max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
+                errors.phoneNumber && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434] focus:border-[#ff3b3b]`
+              )}
+              {...register("phoneNumber")}
+              />
+              {errors.phoneNumber && (
+                <div
+                className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
+              
+                max-[700px]:text-[13px] max-[700px]:bottom-[12px]"
+                >
+                  {errors.phoneNumber?.message}
+                </div>
+              )}
+    
+              <input
+              type="text"
+              placeholder="شماره ملی"
+              className={cn(
+                `focus:outline-none focus:border-[#989898]  block pr-[14px] bg-transparent w-[100%] h-[55px] border-[1px] border-solid border-[#C8C8C8] text-[#666] rounded-[50px] text-[20px] mb-[30px]
+                    
+                max-[700px]:mb-[15px] max-[700px]:h-[50px] max-[700px]:text-[18px]`,
+                errors.idNumber && `border-[#ff3b3b] text-[#ff3b3b] placeholder-[#ff3434] focus:border-[#ff3b3b]`
+              )}
+              {...register("idNumber")}
+              />
+              {errors.idNumber && (
+                <div
+                className="relative bottom-[25px] text-[#ff1f1f] right-[10px]
+              
+                max-[700px]:text-[13px] max-[700px]:bottom-[12px]"
+                >
+                  {errors.idNumber?.message}
+                </div>
+              )}
+    
+              <button
+              className="bg-[#505050] cursor-pointer rounded-[50px] text-[18px] text-white w-[100%] p-[10px_0] mb-[20px]
+              
+              max-[700px]:p-[7px_0]"
+              type="submit"
+              > ثبت نام </button>
+
+              <button
+              onClick={handleBack}
+              className="cursor-pointer rounded-[50px] text-[18px] text-[#505050] p-[10px] m-[0_auto]
+              
+              max-[700px]:p-[7px_0]"
+              > مرحله قبل  </button>
+    
+            </form>
+
+          </>
+        )}
+
       </div>
     </div>
   );
